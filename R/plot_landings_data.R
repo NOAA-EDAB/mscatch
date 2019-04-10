53c6f95fb5d0f8dded02eac0a8215dc783036b61
andybeet <22455149+andybeet@users.noreply.github.com>
2019-04-10 15:39:51 -0400

    test_aggregation : Aggregated by gears + plots

--- R/plot_landings_data.R
+++ R/plot_landings_data.R
@@ -6,16 +6,24 @@
 #'
 #'
 #'
-#' @importFrom ggplot2 "aes" "geom_bar" "theme" "element_text" "ylab" "xlab"
+#' @importFrom ggplot2 "aes" "geom_bar" "geom_col" "theme" "element_text" "ylab" "xlab"
 #'
 #' @export
 
 plot_landings_data <- function(data,dataType,threshold) {
 
-  g1 <- ggplot2::ggplot() +
-    geom_bar(data, mapping = aes(x=YEAR,y=landings_land),stat="identity") +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  # data <- sampleData_147
+  # dataType <- "NEGEAR"
+  # threshold <- 100
 
-  g1
+  newD <- data %>% group_by(YEAR,.dots=dataType)%>%summarise(totLand=sum(landings_land,na.rm = TRUE))
+
+  g1 <- ggplot2::ggplot() +
+    geom_col(newD, mapping = aes_string(x="YEAR", y="totLand", fill = dataType)) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    labs(title = paste0("landings by ",dataType," using a threshold of ",threshold)) +
+    ylab("Total Landings (lbs)")
+
+  print(g1)
 
 }
--- R/test_aggregation.R
+++ R/test_aggregation.R
@@ -2,7 +2,8 @@
 #'
 #'This will all change so no point documenting it too much
 #'
-#'@param landingsThreshold vector (length 2). minimum landings (\%) and minimum sample size to avoid combination of data. Default = c(90,1)
+#'@param landingsThreshold numeric scalar (proportion). Minimum proportion of cumulative landings to avoid aggregation of gear. Default = .9
+#'@param nLengthSamples numeric scalar. The minimum number of length sample sizes required to avoid combination of data. Dfault = 1
 #'
 #'
 #'@importFrom magrittr "%>%"
@@ -11,16 +12,34 @@
 
 #channel <- cfdbs::connect_to_database("sole","abeet") #eventually remove this
 
-test_aggregation <- function(landingsThreshold=c(90,1)) {
+test_aggregation <- function(landingsThreshold = .90, nLengthSamples = 1) {
 
   # sample Data is Cod data. One variable "sampleData"
   #load(paste0(here::here("data"),"/sampleData_147.Rdata"))
   landings <- sampleData_147
   # Now deal with Gary's schematic.
-  # 1 . combine gears. look at
+  # 1 . combine gears. look at gears contributing to top threshold % of landings
   numGears <- length(unique(landings$NEGEAR))
 
-  plot_landings_data(landings,"NEGEAR",landingsThreshold[1])
+  plot_landings_data(landings,"NEGEAR","100%")
+
+  # find gears that make up "threshold" % of catch overall. Valid assumption?
+  # totl landings by group
+  aggTopPercent <- landings %>% group_by(NEGEAR) %>% summarise(totalLandings = sum(landings_land, na.rm = TRUE)) %>% arrange(desc(totalLandings))
+  # convert to % of total and reorder
+  aggTopPercent <- mutate(aggTopPercent,cumsum=cumsum(totalLandings),percent=cumsum/sum(totalLandings))
+  # select the gear that make up at least threshold %
+  nGearsChosen <- dim(aggTopPercent %>% filter(percent<= landingsThreshold) %>% select(NEGEAR))[1]
+  gearsChosen <- aggTopPercent$NEGEAR[nGearsChosen + 1]
+  filteredLandings <- landings %>% filter(NEGEAR %in% gearsChosen)
+  theRestLandings <- landings %>% filter(!(NEGEAR %in% gearsChosen))
+  theRestLandings$NEGEAR <- "998"
+  theRestLandings <- theRestLandings %>% group_by(YEAR,QTR,NEGEAR,MARKET_CODE) %>%
+    summarize(landings_land=sum(landings_land),landings_nn=sum(landings_nn),len_totalNumLen= sum(len_totalNumLen),len_numLengthSamples=sum(len_numLengthSamples))
+
+  filteredLandings <- rbind(filteredLandings,theRestLandings)
+
+  plot_landings_data(filteredLandings,"NEGEAR",paste0(landingsThreshold*100,"%"))
 
   # for (ryear in min(sampleData$YEAR):max(sampleData$YEAR)) {
   #
