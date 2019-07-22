36f70ae35bb8e5a44664bb8f2f5bc4d9a37c795c
andybeet <22455149+andybeet@users.noreply.github.com>
2019-07-22 14:12:30 -0400

    tidy test pull code

--- data-raw/test_data_pull.R
+++ data-raw/test_data_pull.R
@@ -1,5 +1,8 @@
 #' sample Data pull
 #'
+#'Sample data in the format we'll need from proper data pulls using Seans comland script.
+#'Landings pulled from "MV_CF_Landings", lengths pulled from "mv_cf_len"
+#'
 #'All missing areas need to dealt with prior to pulling species data.
 #'Not the case in this sample. We aggregate all data as if from one EPU
 #'
@@ -17,20 +20,35 @@
 test_data_pull <- function(channel,species=164744,species_itis = T){ # species = 147
   library(magrittr)
 
-  # pull sample landings data and massage it
+  ################ pull sample landings data and massage it
+  #############################################################################################################
   testDataPullLandings <- cfdbs::get_landings(channel,year="all",species=species,species_itis=species_itis)
   ## for test data assume we this is EPU data. To achive this we just sum over AREAS for now
-  lands <- testDataPullLandings$data %>% dplyr::group_by(YEAR, MONTH, NEGEAR, MARKET_CODE) %>% dplyr::summarize(landings=sum(as.numeric(SPPLNDLB)),n=dplyr::n())
+  lands <- testDataPullLandings$data %>%
+    dplyr::group_by(YEAR, MONTH, NEGEAR, MARKET_CODE) %>%
+    dplyr::summarize(landings=sum(as.numeric(SPPLNDLB)),n=dplyr::n())
   lands <- dplyr::mutate(lands,QTR = as.character(ceiling(as.numeric(MONTH)/3 )))
-  sampleLandings <- lands %>% dplyr::group_by(YEAR,QTR,NEGEAR, MARKET_CODE) %>% dplyr::summarize(landings_land = sum(landings),landings_nn=sum(n))
-  # this needs to be checked
-  sampleLandings <- sampleLandings %>% dplyr::select_all() %>% dplyr::filter(QTR != "0")
 
+  # aggregate landings by variables and count the number of trips
+  sampleLandings <- lands %>%
+    dplyr::group_by(YEAR,QTR,NEGEAR, MARKET_CODE) %>%
+    dplyr::summarize(landings_land = sum(landings),landings_nn=sum(n))
+  # this needs to be checked.
+  # filter all entries labelled quarter = 0
+  sampleLandings <- sampleLandings %>%
+    dplyr::select_all() %>%
+    dplyr::filter(QTR != "0")
+
+  #############################################################################################################
   # pull sample length data and massage it
   testDataPullLength <- cfdbs::get_landings_length(channel,year="all",species=species,species_itis=species_itis)
   # create unique tripid since NUMSAMP is replicated for each species reported within a trip
-  lengths <- testDataPullLength$data %>% dplyr::mutate(tripid = paste0(PERMIT,YEAR,MONTH,DAY))
-  lengthsData <- lengths %>% dplyr::group_by(YEAR, QTR, NEGEAR, MARKET_CODE) %>% dplyr::summarize(len_totalNumLen=sum(as.numeric(NUMLEN)),len_numLengthSamples=length(unique(tripid)))
+  lengths <- testDataPullLength$data %>%
+    dplyr::mutate(tripid = paste0(PERMIT,YEAR,MONTH,DAY))
+  # aggregate
+  lengthsData <- lengths %>%
+    dplyr::group_by(YEAR, QTR, NEGEAR, MARKET_CODE) %>%
+    dplyr::summarize(len_totalNumLen=sum(as.numeric(NUMLEN)),len_numLengthSamples=length(unique(tripid)))
 
   # full join of tables by common fields
   sampleData <- as.data.frame(dplyr::full_join(sampleLandings,lengthsData, by=c("YEAR","QTR","NEGEAR","MARKET_CODE")))
