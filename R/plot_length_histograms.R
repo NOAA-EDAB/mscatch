d53f0a39cd2150a6934e2d05f52c68a5d544b2c8
andybeet <22455149+andybeet@users.noreply.github.com>
2019-07-22 17:20:25 -0400

    additional plot - histogram of gear/market_code combo

--- R/plot_length_histograms.R
+++ R/plot_length_histograms.R
@@ -18,14 +18,26 @@ plot_length_histogram <- function(lengthData,species_itis,outputDir,outputPlots)
 
   if (outputPlots == F) return()
 
+  lengthDataGEARS <- lengthData %>% dplyr::group_by(NEGEAR,MARKET_CODE,LENGTH) %>% dplyr::summarise(numlens=sum(as.numeric(NUMLEN)))
 
-  png(paste0(outputDir,"/5_market_category_lengths.png"))
-  p <- ggplot(data = lengthData) +
-    geom_bar(mapping = aes(x=LENGTH),na.rm=T) +
-    facet_wrap(~MARKET_CODE,scales="free_y",nrow=length(unique(lengthData$MARKET_CODE)),ncol=1) +
+  png(paste0(outputDir,"/5_market_category_lengths_by_gear.png"))
+
+  p <- ggplot(data = lengthDataGEARS) +
+    geom_bar(stat="identity",mapping = aes(x=LENGTH,y=numlens),na.rm=T) +
+    facet_wrap(~NEGEAR+MARKET_CODE,scales="free_y",nrow=2, ncol = length(unique(lengthData$MARKET_CODE))) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
     scale_x_discrete(name="Length (cm)", breaks=seq(0, max(lengthData$LENGTH),10))
   print(p)
+  dev.off()
+
+  png(paste0(outputDir,"/6_market_category_lengths.png"))
+  p <- ggplot(data = lengthDataGEARS) +
+    geom_bar(stat="identity",mapping = aes(x=LENGTH,y=numlens),na.rm=T) +
+    facet_wrap(~MARKET_CODE,scales="free_y",nrow = length(unique(lengthData$MARKET_CODE)),ncol=1) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    scale_x_discrete(name="Length (cm)", breaks=seq(0, max(lengthData$LENGTH),10))
+  print(p)
+
   dev.off()
 
 }
