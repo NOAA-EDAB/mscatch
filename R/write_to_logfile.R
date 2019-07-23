662af04becfc339927fc21886733944547ab1c7b
andybeet <22455149+andybeet@users.noreply.github.com>
2019-07-23 15:44:09 -0400

    logfile for describing decisions and/or user input

---
+++ R/write_to_logfile.R
@@ -0,0 +1,36 @@
+#' Records entries to a logfile
+#'
+#'@param outputDir Character string. directory to save output
+#'@param logfile Character string. Name of logfile
+#'@param data data to write to file
+#'@param label Character string. Description of the data being written
+#'@append Boolean. append to current logfile (T) or overwrite (F)
+#'
+#'@export
+
+
+write_to_logfile <- function(outputDir,logfile,data,label,append = T) {
+
+  if (append == F){ # open file
+    fileConn<-file(paste0(outputDir,"/",logfile),open="w")
+  } else (
+    fileConn<-file(paste0(outputDir,"/",logfile),open="a")
+
+  )
+
+  writeLines(label, fileConn)
+
+  if(is.vector(data)){
+    cat(data, file=fileConn)
+  } else {
+
+    for (irow in 1:nrow(data)){
+      cat(data[irow,], file=fileConn, append=T)
+      cat("\n", file=fileConn, append=T)
+    }
+
+  }
+
+  close(fileConn)
+
+}
