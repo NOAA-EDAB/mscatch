33694b8d380cacb50b7800087388c4dbb6639dd5
andybeet <22455149+andybeet@users.noreply.github.com>
2019-07-23 15:43:17 -0400

    plot function for market code landings & number of samples

---
+++ R/plot_market_codes.R
@@ -0,0 +1,38 @@
+#' plots landings by market code
+#'
+#' Plots landings, number of samples for all market_codes found in landings data
+#'
+#'
+#'@param marketData data frame.
+#'@param outputDir Character string. Path to output directory (png files saved here)
+#'@param outputPlots Boolean. Should plots be created. T or F
+
+#'
+#'@importFrom ggplot2 "ggplot" "aes" "geom_bar" "geom_histogram" "facet_wrap" "element_text" "ylab" "xlab" "labs" "scale_x_discrete"
+#'@importFrom dplyr "summarize" "summarise" "group_by"
+#'@importFrom magrittr "%>%"
+#'
+#' @export
+
+plot_market_codes <- function(marketData,outputDir,outputPlots){
+
+  if (outputPlots == F) return()
+
+  png(paste0(outputDir,"/7_market_category_landings.png"))
+
+  # landings grob
+  pLand <- ggplot2::ggplot(data = marketData) +
+    geom_bar(mapping = ggplot2::aes(x=MARKET_CODE,y=totalLandings),stat="identity")
+  # num samples grob
+  pSamp <- ggplot2::ggplot(data = marketData) +
+    geom_bar(mapping = ggplot2::aes(x=MARKET_CODE,y=len_numLengthSamples),stat="identity")
+  # data grob
+  table <- gridExtra::tableGrob(marketData)
+  # layout of plot
+  lay <- rbind(c(1,2),
+               c(3,3))
+  gridExtra::grid.arrange(pLand,pSamp,table,layout_matrix=lay,as.table=T)
+
+  dev.off()
+
+}
