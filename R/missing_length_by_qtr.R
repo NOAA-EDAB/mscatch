8460a737cf42dfdcfbf745c1105c7dd6399efc96
andybeet <22455149+andybeet@users.noreply.github.com>
2019-08-27 16:57:53 -0400

    update missing_length_by_qrt - check for empty tibble

--- R/missing_length_by_qtr.R
+++ R/missing_length_by_qtr.R
@@ -3,14 +3,22 @@
 #' Obtain the YEAR and QTR length samples to be used for missing YEAR/QTR data
 #'
 #'
-#' Internal function
+#' @export
 
 missing_length_by_qtr <- function(QTRData,missingQTRs,iyear) {
 
-  numSamples <- QTRData %>% dplyr::filter(YEAR==(missingQTRs$YEAR[iyear]-1) & QTR==missingQTRs$QTR[iyear]) %>%
+  print(QTRData)
+  print(missingQTRs)
+  print(missingQTRs$YEAR[iyear])
+  numSamples <- QTRData %>%
+    dplyr::filter(YEAR==(missingQTRs$YEAR[iyear]-1) & QTR==missingQTRs$QTR[iyear]) %>%
     dplyr::select(YEAR,QTR,len_totalNumLen,len_numLengthSamples)
 
-  if (numSamples$len_numLengthSamples == 0) {
+  print(numSamples)
+  if (dim(numSamples)[1] == 0)  { # empty tibble or zero length samples
+    # look anothe year back
+    numSamples <- missing_length_by_qtr(QTRData,missingQTRs,iyear-1)
+  } else if (numSamples$len_numLengthSamples == 0) {
     # previous year also has no samples need to look back another year
     numSamples <- missing_length_by_qtr(QTRData,missingQTRs,iyear-1)
   }
