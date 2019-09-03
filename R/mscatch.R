b282919a140db68d66fcb4fe25622b6aef8fae24
andybeet <22455149+andybeet@users.noreply.github.com>
2019-09-03 15:38:35 -0400

    added mscatch.r - package function documetation

---
+++ R/mscatch.R
@@ -0,0 +1,19 @@
+#' mscatch: landings data manipulation
+#'
+#'Tools to aid in extraction, manipulation of data from the cfdbs database
+#'
+#'\itemize{
+#'\item Pull spatial data from cfdbs and aggregate to stat area/EPU.
+#'\item Methods for assigning trips to missing areas
+#'\item Methods for assigning trips to EPUs when 10 min square is missing
+#'\item Methods for aggregating landings based on length sample frequency (as for single species assessments)
+#'\item Methods for distributing landings by length/weight (by using length-weight keys)
+#'\item + more
+#'}
+#'
+#'To learn more about using \code{mscatch}, start with the vignette: \code{browseVignettes(package="mscatch")} or click the index below
+#'
+#'
+#' @docType package
+#' @name mscatch
+NULL
