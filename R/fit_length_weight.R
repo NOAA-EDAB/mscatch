af84720627843988fc073acc9f18d9c0b6ff8bf2
andybeet <22455149+andybeet@users.noreply.github.com>
2019-09-04 16:07:37 -0400

    added fit_length_weight.r

---
+++ R/fit_length_weight.R
@@ -0,0 +1,15 @@
+#' Fit length weight relationship to species data from SVDBS
+#'
+#'Fits a length weight relationship for use in length expansion
+#'
+#'@param lengthWeight Data frame. length-weight pairs. Each row represents an individual fish
+#'
+#'@return Vector of parameters
+#'\item{a}{}
+#'\item{b}{}
+#'
+#' @export
+
+fit_length_weight <- function(){
+
+}
