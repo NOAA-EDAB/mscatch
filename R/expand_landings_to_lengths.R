554d73d260799bfc099cda289d575b1a32bf9fda
andybeet <22455149+andybeet@users.noreply.github.com>
2019-09-12 12:29:40 -0400

    documetation update - expand_landings_to_length (help) - fit_length_weight (help)

--- R/expand_landings_to_lengths.R
+++ R/expand_landings_to_lengths.R
@@ -2,15 +2,25 @@
 #'
 #'@param landingsData Tibble. Aggregated landings data. YEAR, QTR, NEGEAR, MARKET_CODE,landings_land (metric tonnes), landings_nn (# trips), len_totalNumLen (# fish lengths), len_numLengthSamples (# independent samples).
 #'@param lengthData Tibble. Aggregated length data. YEAR, QTR, NEGEAR, MARKET_CODE, LENGTH (length of fish), NUMLEN (# fish at LENGTH)
-#'@param lengthWeightKey how define this? one over all time, time varying?
+#'@param lengthWeightParams List. alpha = intercept, betas = slope(s), var = residual variance used to formulate the mean (?see Notes section below)
 #'
 #'
-#'@return Tibble
+#'@return A Tibble with same columns as \code{landingsData}
 #'\item{expandedLandings}{landingsData expanded to represent weight of landings by length}
+#'
+#'@section Notes:
+#'
+#'The length weight relationship (see \code{\url{fit_length_weight}}) was fit assuming log normal errors (normal on the log scale)
+#'Therefor when exponentiation we need to correct for the mean:
+#'
+#'E(W) = \eqn{ L^\beta exp(\alpha + \sigma^2 / 2)}
+#'
+#'
 #'
 #'@export
 
-expand_landings_to_lengths <- function(landingsData,lengthData,lengthWeightKey){
+expand_landings_to_lengths <- function(landingsData,lengthData,lengthWeightParams){
+  print(lengthWeightParams)
 
   return()
 }
