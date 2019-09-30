bf47e10893df0aaa6a7fa861e497e75bf0e10b41
andybeet <22455149+andybeet@users.noreply.github.com>
2019-09-30 17:49:55 -0400

    add create_filter_expression.R - general function to create expression from a dataframe to pass to dplyr::filter function

---
+++ R/create_filter_expression.R
@@ -0,0 +1,26 @@
+#'create an expression to use in dplyr::filter statement
+#'
+#'@param landingsRecord Tibble (1 x n). Single row from landingsData
+#'@param categories Character vector. Names of Variables to filter over ("YEAR","QTR","NEGEAR", etc)
+#'
+#'@return An expression
+#'
+#'@section Notes:
+#'
+#'Values are access from the \code{landingsRecord} using the \code{categories} and strung together to make an expression in the form of
+#'labe == value & label = value etc.
+#'
+#'
+#'@export
+
+create_filter_expression <- function(landingsRecord,categories){
+
+  dataF <- landingsRecord %>% dplyr::select(categories)
+
+  filterExpression <- purrr::imap(dataF, ~ if(is.character(.x)) sprintf('%s == "%s"', .y, .x)
+                           else sprintf('%s == %s', .y, .x)) %>%
+    purrr::reduce(stringr::str_c,sep =" & ") %>%
+    parse(text = .)
+
+  return(filterExpression)
+}
