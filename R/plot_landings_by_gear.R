b149e7286e9603ce5a79cb082e1b5bf0427b2590
andybeet <22455149+andybeet@users.noreply.github.com>
2019-11-01 17:20:36 -0400

    additional plotting function for landings by gear

---
+++ R/plot_landings_by_gear.R
@@ -0,0 +1,32 @@
+#' plot landings by gear
+#'
+#' Select the variable to plot.
+#' Eventually have the option to print to file as diagnostics
+#'
+#'
+#'
+#'
+#'
+#'
+#' @export
+
+plot_landings_by_gear <- function(species,data,threshold,outputPlots,outputDir,plotID) {
+
+
+  if (outputPlots == F) return()
+
+  png(paste0(outputDir,"/",plotID,"_market_category_lengths_by_gear.png"))
+
+  newD <- data %>% dplyr::group_by(YEAR,NEGEAR) %>% dplyr::summarise(totLand=sum(landings_land,na.rm = TRUE))
+
+  g1 <- ggplot2::ggplot(newD) +
+    ggplot2::geom_col(mapping = ggplot2::aes(x=YEAR, y=totLand, fill = NEGEAR)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)) +
+    ggplot2::labs(title = paste0("Landings of ",species," by NEGEAR, using a threshold of ",threshold)) +
+    ggplot2::ylab("Total Landings (lbs)")
+
+  print(g1)
+
+  dev.off()
+
+}
