a794650e9afed1668c25757258319f07ccb6b3ae
andybeet <22455149+andybeet@users.noreply.github.com>
2019-11-12 09:28:03 -0500

    check to ensure length samples are available for test

--- R/compare_length_distributions.R
+++ R/compare_length_distributions.R
@@ -25,12 +25,16 @@ compare_length_distributions <- function(landings,lengthData,variableToAggregate
       if (acode == bcode) next # dont test against self
       if ((acode =="UN") | (bcode == "UN")) next # leave unclassified alone
 
-     #     print(paste0(acode,"_",bcode)      )
+     #`!!` unquotes argument
       sizeA <- lengthData %>% dplyr::select(!!groupBy) %>% dplyr::filter(UQ(as.name(variableToAggregate)) == acode)
       sizeA <- as.numeric(rep(sizeA$LENGTH,sizeA$NUMLEN))
       sizeB <- lengthData %>% dplyr::select(!!groupBy) %>% dplyr::filter(UQ(as.name(variableToAggregate)) == bcode)
       sizeB <- as.numeric(rep(sizeB$LENGTH,sizeB$NUMLEN))
 
+      if ((length(sizeA) < 1) | (length(sizeB)< 1)) {
+        write_to_logfile(outputDir,logfile,paste(" Not enough length Samples to compare ",acode,"with",bcode,"\n"),label="ks test aggregation",append = T)
+        next
+      }
       res <- ks.test(sizeA,sizeB)
 
       if(res$p.value > pValue) {  # length distributions are the same
