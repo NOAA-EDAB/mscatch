76c449f04b254a143af155966e6f8e4ec6efd28a
andybeet <22455149+andybeet@users.noreply.github.com>
2021-05-18 11:03:33 -0400

    generalized joining of landings and length based on fields presen - expand header documentation to describe output - only export expanded landings

--- R/expand_landings_to_lengths.R
+++ R/expand_landings_to_lengths.R
@@ -4,8 +4,14 @@
 #'@param lengthData Tibble. Aggregated length data. YEAR, QTR, NEGEAR, MARKET_CODE, LENGTH (length of fish), NUMLEN (# fish at LENGTH)
 #'@param lengthWeightParams List. alpha = intercept, betas = slope(s), var = residual variance used to formulate the mean (?see Notes section below)
 #'
-#'@return A Tibble with same columns as \code{landingsData}
-#'\item{expandedLandings}{landingsData expanded to represent weight of landings by length}
+#'@return A Tibble of expanded landings to represent weight of landings by length
+#'\item{YEAR}{Year of landings}
+#'\item{NEGEAR}{3 digit gear code as defined in cfdbs.gear}
+#'\item{QTR}{Quarter year of landings (The presence of this field depends on whether it was present in the \code{landingsData}}
+#'\item{MARKET_CODE}{Market code assigned to landed fish (The presence of this field depends on whether it was present in the \code{landingsData}}
+#'\item{LENGTH}{Length of sampled fish}
+#'\item{NUMLEN}{number of sampled for fish stated LENGTH}
+#'\item{weight}{expanded weight of all fish of given LENGTH in YEAR, NEGEAR etc..}
 #'
 #'@section Notes:
 #'
@@ -27,14 +33,30 @@
 
 expand_landings_to_lengths <- function(landingsData,lengthData,lengthWeightParams){
 
+  # determine fields to join by. MARKET_CODE & QTR may be missing
+  mainList <- c("YEAR","QTR","NEGEAR","MARKET_CODE")
+  varsPresent <- mainList[mainList %in% names(landingsData) ]
+
   # join tables together for length and landings
   #joined <- dplyr::left_join(lengthData,landingsData,by=c("YEAR","QTR","NEGEAR","MARKET_CODE"))
-  joined <- dplyr::left_join(landingsData,lengthData,by=c("YEAR","QTR","NEGEAR","MARKET_CODE"))
+  joined <- dplyr::left_join(landingsData,lengthData,by=varsPresent)
   # atribute weight to each fish in group and scale up by "expansion factor
-  master <- joined %>%
-    dplyr::group_by(YEAR,QTR,NEGEAR,MARKET_CODE) %>%
+  landingsExpanded <- joined %>%
+    dplyr::group_by(dplyr::across(varsPresent)) %>%
     dplyr::mutate(weight = expand_to_weight(LENGTH,NUMLEN,landings_land,lengthWeightParams)) %>%
-    dplyr::select(YEAR,QTR,NEGEAR,MARKET_CODE,LENGTH,NUMLEN,weight)
+    dplyr::select(!!varsPresent,LENGTH,NUMLEN,weight) %>%
+    dplyr::ungroup()
 
-  return(list(landings = landingsData,lengthData = master))
+
+  # join tables together for length and landings
+  #joined <- dplyr::left_join(lengthData,landingsData,by=c("YEAR","QTR","NEGEAR","MARKET_CODE"))
+  # joined <- dplyr::left_join(landingsData,lengthData,by=varsPresent)
+  # # atribute weight to each fish in group and scale up by "expansion factor
+  # master <- joined %>%
+  #   dplyr::group_by(YEAR,QTR,NEGEAR,MARKET_CODE) %>%
+  #   dplyr::mutate(weight = expand_to_weight(LENGTH,NUMLEN,landings_land,lengthWeightParams)) %>%
+  #   dplyr::select(YEAR,QTR,NEGEAR,MARKET_CODE,LENGTH,NUMLEN,weight) %>%
+  #   dplyr::ungroup()
+
+  return(landingsExpanded)
 }
