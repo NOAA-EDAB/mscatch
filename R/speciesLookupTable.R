a46bdbccf835f5614350dc79365a3eb5e780d0e5
andybeet <22455149+andybeet@users.noreply.github.com>
2021-12-13 18:36:55 -0500

    speciescode lookup table for select species of interest. nespp3, svspp, itis

---
+++ R/speciesLookupTable.R
@@ -0,0 +1,15 @@
+#' A lookup table for species codes
+#'
+#'Species of interest along with species codes
+#'
+#' @format A dataframe with 6 columns
+#' \describe{
+#'   \item{SCIENTIFIC_NAME}{scientific name for species}
+#'   \item{SPECIES_ITIS}{define this}
+#'   \item{COMMON_NAME.x}{Common name for species}
+#'   \item{NESPP3}{US North east 3 digit species code for commercially landed species}
+#'   \item{COMMON_NAME.x}{Common name for species}
+#'   \item{SVSPP}{US North east 3 digit species code for survey species}
+#'}
+#'
+"speciesLookupTable"
