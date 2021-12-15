21e999bfec84b308b88974ea9028a0b2e1339589
andybeet <22455149+andybeet@users.noreply.github.com>
2021-12-15 17:01:26 -0500

    figure name typos, syntax typo package description update

--- R/mscatch.R
+++ R/mscatch.R
@@ -1,14 +1,12 @@
-#' mscatch: landings data manipulation
+#' mscatch: Expanding total catch for species by length composition
 #'
-#'Tools to aid in extraction, manipulation of data from the cfdbs database
+#' Tools to aid in aggregation of catch data and expanding catch data to weight by length composition
+#'
 #'
 #'\itemize{
-#'\item Pull spatial data from cfdbs and aggregate to stat area/EPU.
-#'\item Methods for assigning trips to missing areas
-#'\item Methods for assigning trips to EPUs when 10 min square is missing
-#'\item Methods for aggregating landings based on length sample frequency (as for single species assessments)
+#'\item Pull spatial data from cfdbs and aggregate
+#'\item Methods for aggregating landings based on length sample frequency
 #'\item Methods for distributing landings by length/weight (by using length-weight keys)
-#'\item + more
 #'}
 #'
 #'To learn more about using \code{mscatch}, start with the vignette: \code{browseVignettes(package="mscatch")} or click the index below
