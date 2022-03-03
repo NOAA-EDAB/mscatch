a42f82621a8e866abe6778a5d60cf157f6c7aa44
andybeet <22455149+andybeet@users.noreply.github.com>
2022-03-03 13:09:53 -0500

    passes new argument indicating time to aggregate over

--- R/update_length_samples.R
+++ R/update_length_samples.R
@@ -1,6 +1,6 @@
 #' update missing length values
 #'
-#' For each YEAR/QRT/NEGEAR/MARKET_CODE with missing length samples we update with previous years data.
+#' For each YEAR/QRT(SEMESTER)/NEGEAR/MARKET_CODE with missing length samples we update with previous years data.
 #' No aggregation is undertaken. Simply populating empty cells or adding length data to previously omitted cells.
 #'
 #' @param data List. landings and lengthData
@@ -9,6 +9,7 @@
 #' @param mainGearType Character string. Name of NEGEAR for which samples are borrowed from
 #' @param marketCode Character string. Name of MARKET_CODE
 #' @param numSamples Tibble. Filtered landings data (YEAR, QTR, len_totalNumLen, len_numLengthSamples) by NEGEAR, MARKET_CODE
+#' @param TIME Character String. Either "QTR" or "SEMESTER" to denote at what time scale to update
 #'
 ##' @return List of landings and associated length samples .Same as input data
 #' \item{landings}{Tibble (n x 8). Aggregated landings data. YEAR, QTR, NEGEAR, MARKET_CODE,landings_land (metric tonnes), landings_nn (# trips), len_totalNumLen (# fish lengths), len_numLengthSamples (# independent samples) }
@@ -16,11 +17,11 @@
 #'
 #'@noRd
 
-update_length_samples <- function(data,missingRow,gearType,marketCode,numSamples,mainGearType = NULL) {
+update_length_samples <- function(data,missingRow,gearType,marketCode,numSamples,mainGearType = NULL,TIME = "QTR") {
 
   # pick out row with missing data in LANDINGS
   ind <- (data$landings$YEAR == missingRow$YEAR) &
-    (data$landings$QTR == missingRow$QTR) &
+    (data$landings[[TIME]] == missingRow[[TIME]]) &
     (data$landings$NEGEAR == gearType) &
     (data$landings$MARKET_CODE == marketCode)
 
@@ -32,10 +33,10 @@ update_length_samples <- function(data,missingRow,gearType,marketCode,numSamples
   # since no lengths exist for this YEAR/QTR/NEGEAR/MARKET_CODE duplicate rows from year to use
   if (is.null(mainGearType)) {
     newLengthData <- data$lengthData %>%
-      dplyr::filter(YEAR==numSamples$YEAR & QTR==numSamples$QTR & NEGEAR==gearType & MARKET_CODE==marketCode)
+      dplyr::filter(YEAR==numSamples$YEAR & get(TIME)==numSamples[[TIME]] & NEGEAR==gearType & MARKET_CODE==marketCode)
   } else { # borrow from different gear
     newLengthData <- data$lengthData %>%
-      dplyr::filter(YEAR==numSamples$YEAR & QTR==numSamples$QTR & NEGEAR==mainGearType & MARKET_CODE==marketCode)
+      dplyr::filter(YEAR==numSamples$YEAR & get(TIME)==numSamples[[TIME]] & NEGEAR==mainGearType & MARKET_CODE==marketCode)
     #newLengthData <- newLengthData %>% dplyr::mutate(NEGEAR = replace(NEGEAR,NEGEAR==gearType,mainGearType))
     # now replace the data for mainGearType to data for current gear type
     newLengthData$NEGEAR <- gearType
