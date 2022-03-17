7bbea3df00be7615c0373167fd27232267811664
andybeet <22455149+andybeet@users.noreply.github.com>
2022-03-17 16:15:59 -0400

    Added herring to species object

--- R/get_species_object.r
+++ R/get_species_object.r
@@ -9,7 +9,7 @@
 get_species_object <- function(species_itis) {
 
 
-  if (species_itis == 172909) {
+  if (species_itis == 172909) { # Yellowtail
     speciesRules <- list()
     speciesRules$species_itis <- 172909
     speciesRules$speciesName <- mscatch::speciesLookupTable %>%
@@ -20,11 +20,13 @@ get_species_object <- function(species_itis) {
       dplyr::filter(SPECIES_ITIS == speciesRules$species_itis) %>%
       dplyr::distinct(SVSPP) %>%
       dplyr::pull()
-    speciesRules$marketCodes <- data.frame(use = c("LG","SM","SM","SM","UN"),combine=c("LG","SQ","MD","PW","all"))
-    speciesRules$gearCodes <- data.frame(use = c("050"),combine=c("all"))
+    speciesRules$marketCodes <- data.frame(use = c("LG","SM","SM","SM","UN"),
+                                           combine=c("LG","SQ","MD","PW","all"))
+    speciesRules$gearCodes <- data.frame(use = c("050"),
+                                         combine=c("all"))
   }
 
-  if (species_itis == 172414 ) {
+  if (species_itis == 172414 ) { #Mackerel
     speciesRules <- list()
     speciesRules$species_itis <- 172414
     speciesRules$speciesName <- mscatch::speciesLookupTable %>%
@@ -35,9 +37,30 @@ get_species_object <- function(species_itis) {
       dplyr::filter(SPECIES_ITIS == speciesRules$species_itis) %>%
       dplyr::distinct(SVSPP) %>%
       dplyr::pull()
-    speciesRules$marketCodes <- data.frame(use = c("LG","LG","LG","MD","SM","SM","SM","UN"),combine=c("LG","XG","JB","MD","SQ","ES","SV","all"))
-    speciesRules$gearCodes <- data.frame(use = c("050"),combine=c("all"))
+    speciesRules$marketCodes <- data.frame(use = c("LG","LG","LG","MD","SM","SM","SM","UN"),
+                                           combine=c("LG","XG","JB","MD","SQ","ES","SV","all"))
+    speciesRules$gearCodes <- data.frame(use = c("050"),
+                                         combine=c("all"))
   }
+
+  if (species_itis == 161722 ) { #Herring
+    speciesRules <- list()
+    speciesRules$species_itis <- 161722
+    speciesRules$speciesName <- mscatch::speciesLookupTable %>%
+      dplyr::filter(SPECIES_ITIS == speciesRules$species_itis) %>%
+      dplyr::distinct(COMMON_NAME.y) %>%
+      dplyr::pull()
+    speciesRules$SVSPP <- mscatch::speciesLookupTable %>%
+      dplyr::filter(SPECIES_ITIS == speciesRules$species_itis) %>%
+      dplyr::distinct(SVSPP) %>%
+      dplyr::pull()
+    speciesRules$marketCodes <- data.frame(use = c("UN"),
+                                           combine=c("all"))
+    speciesRules$gearCodes <- data.frame(use = c("170","170","170","170","170","170","050"),
+                                         combine=c("170","120","121","122","124","370","all"))
+  }
+
+
 
 
   return(speciesRules)
