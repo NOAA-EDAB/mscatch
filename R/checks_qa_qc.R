9d7d6da40997f16755f10494197f8b4ade58e657
andybeet <22455149+andybeet@users.noreply.github.com>
2022-03-29 17:10:40 -0400

    moved data checks and qaqc into own function

---
+++ R/checks_qa_qc.R
@@ -0,0 +1,47 @@
+#' Aggregates data to Time interval and summarizes
+#'
+#' groups the data by time and summarizes, landings and length sample data after removing NA's
+#'
+#'@param data List. Landings data and length data
+#'@param aggregate_to Character string. Level of aggregation for all MARKET_CODES and gears ("QTR", "YEAR", "SEMESTER", MIX").
+#'
+#'@return List
+#'
+#'\item{landings}{same as input}
+#'\item{lengthData}{Same as input}
+#'
+#'@noRd
+
+checks_qa_qc <- function(landingsData, lengthData, aggregate_to) {
+
+  if (!(aggregate_to %in% c("YEAR","QTR","MIX","SEMESTER"))) {
+    stop(paste0("Aggregation to ",aggregate_to," is not currently implemented. Please create an issue
+                at https://github.com/NOAA-EDAB/mscatch/issues"))
+
+  }
+
+  write_to_logfile(outputDir,logfile,"",label="DECISIONS MADE DURING AGGREGATION OF DATA")
+  write_to_logfile(outputDir,logfile,data=as.character(speciesName),label="Species Name:",append=T)
+  # write function call to log file
+  write_to_logfile(outputDir,logfile,data=deparse(dbutils::capture_function_call()),label="Arguments passed to aggregate_landings:",append=T)
+
+  # cleans landings data and length data of NAs
+  landingsData <- landingsData %>%
+    dplyr::group_by(.data$YEAR,.data$QTR,.data$NEGEAR,.data$MARKET_CODE) %>%
+    dplyr::summarise(landings_land=sum(.data$landings_land, na.rm=T),
+                     landings_nn=sum(.data$landings_nn, na.rm=T),
+                     len_totalNumLen=sum(.data$len_totalNumLen,na.rm=T),
+                     len_numLengthSamples=sum(.data$len_numLengthSamples, na.rm=T),
+                     .groups="drop")
+  lengthData <- lengthData %>%
+    dplyr::group_by(.data$YEAR,.data$QTR,.data$NEGEAR,.data$MARKET_CODE,.data$LENGTH) %>%
+    dplyr::summarise(NUMLEN = sum(as.numeric(.data$NUMLEN),na.rm=T),.groups="drop")
+
+  # create list for data.
+  # landing and lengths will from this point on be parts of a list.
+  data <- list()
+  data$landings <- landingsData
+  data$lengthData <- lengthData
+
+  return(data)
+}
